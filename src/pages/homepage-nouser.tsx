4622d0ca1c1424a2238919d087f37a8723c6ff2d
Camilo-J <josecamilo1902@outlook.com>
2022-12-31 09:38:59 -0500

    Add header componetn to homepage

--- src/pages/homepage-nouser.tsx
+++ src/pages/homepage-nouser.tsx
@@ -1,7 +1,10 @@
 import styled from "@emotion/styled";
 import { useEffect, useState } from "react";
+import Header from "../components/Header";
 import Tweet from "../components/tweet";
+import { useAuth } from "../context/auth-context";
 import { getTweets } from "../services/tweet-services";
+import { typography } from "../styles";
 
 const Container = styled.div`
   height: 100vh;
@@ -12,24 +15,34 @@ const Container = styled.div`
   gap: 1rem;
 `;
 
+const Title = styled.p`
+  ${typography.head.sm}
+`;
+
+const ContainerTweets = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+  gap: 2rem;
+`;
+
 const HomePageNoUser = () => {
   const [tweets, setTweets] = useState([]);
-
+  const { login } = useAuth();
   useEffect(() => {
     getTweets()
       .then((data) => setTweets(data))
       .catch(console.log);
   }, []);
 
-  console.log(tweets);
   return (
     <Container>
-      <p>Home</p>
-      <div>
+      <Header></Header>
+      <Title>Home</Title>
+      <ContainerTweets>
         {tweets.map((elem, index) => (
           <Tweet key={index}></Tweet>
         ))}
-      </div>
+      </ContainerTweets>
     </Container>
   );
 };
