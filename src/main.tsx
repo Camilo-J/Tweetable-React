1c37ab13c3d9fecae1f54f826e376e6b082b6370
Camilo-J <josecamilo1902@outlook.com>
2022-12-31 09:38:09 -0500

    Add routers & context to manage routes

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,17 @@ import { Global } from "@emotion/react";
 
 import App from "./App";
 import { global, reset } from "./styles";
+import { AuthProvider } from "./context/auth-context";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Global styles={reset} />
-    <Global styles={global} />
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <Global styles={reset} />
+        <Global styles={global} />
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
