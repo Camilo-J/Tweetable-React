faf9754bd8654efa02ccddcb621cb9ee81105923
Camilo-J <josecamilo1902@outlook.com>
2022-12-31 18:51:11 -0500

    Add manager to set authenticate o Unathn epage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import reactLogo from "./assets/react.svg";
+import AuthenticatedApp from "./AuthenticatedApp";
+import { useAuth } from "./context/auth-context";
 import HomePageNoUser from "./pages/homepage-nouser";
+import UnauthenticatedApp from "./UnauthenticatedApp";
 
 function App() {
-  return <HomePageNoUser></HomePageNoUser>;
+  const { user } = useAuth();
+  return user ? <AuthenticatedApp /> : <UnauthenticatedApp />;
 }
 
 export default App;
