d46f7c6cfe0baff89ebc39ba63d72ca70c96d3fd
Camilo-J <josecamilo1902@outlook.com>
2023-01-01 09:48:53 -0500

    Refactor styles & Add routes for each page

--- src/UnauthenticatedApp.tsx
+++ src/UnauthenticatedApp.tsx
@@ -1,26 +1,12 @@
 import styled from "@emotion/styled";
-import { MouseEvent, useState } from "react";
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import HomePageNoUser from "./pages/homepage-nouser";
 import LoginForm from "./pages/login-page";
 import SignupForm from "./pages/signup-form";
 import { colors, typography } from "./styles";
 
-const CustomLink = styled("button")`
-  border: none;
-  background: none;
-  cursor: pointer;
-  color: ${colors.blue[500]};
-  &:hover {
-    color: ${colors.gray.medium};
-  }
-`;
-
-const Wrapper = styled.div`
-  width: 328px;
-  height: 428px;
-  background-color: #fff;
-`;
 const ContainerPage = styled.div`
   display: flex;
   flex-direction: column;
@@ -28,41 +14,18 @@ const ContainerPage = styled.div`
   gap: 1rem;
 `;
 
-const TotalWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  text-align: center;
-  font-style: normal;
-  height: 100%;
-  font-weight: 500;
-`;
-
-const Title = styled.h1`
-  ${typography.head.sm}
-`;
-
 function UnauthenticatedApp() {
-  const [showLogin, setShowLogin] = useState(true);
-
-  function handleLinkChange(event: MouseEvent<HTMLButtonElement>) {
-    event.preventDefault();
-    setShowLogin(!showLogin);
-  }
+  const [showLogin, setShowLogin] = useState(false);
 
   return (
     <ContainerPage>
       <Header></Header>
-      <Title>Sign in to your Account </Title>
-      <Wrapper>
-        <TotalWrapper>
-          {showLogin ? <LoginForm /> : <SignupForm />}
-
-          <CustomLink onClick={handleLinkChange}>
-            {showLogin ? "Create Account" : "Sign in"}
-          </CustomLink>
-        </TotalWrapper>
-      </Wrapper>
+      <Routes>
+        <Route path="/" element={<HomePageNoUser />}></Route>
+        <Route path="/sign_up" element={<SignupForm />} />
+        <Route path="/sign_in" element={<LoginForm />} />
+        <Route path="*" element={<HomePageNoUser />}></Route>
+      </Routes>
     </ContainerPage>
   );
 }
